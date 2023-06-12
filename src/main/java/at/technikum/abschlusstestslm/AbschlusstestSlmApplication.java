83f2d9479d0fbef3245ffb363607741dd39b7fcd
Oliver Koller <oliverkoller@Air-von-Oliver.fritz.box>
2023-06-12 15:08:50 +0200

    Initial commit

---
+++ src/main/java/at/technikum/abschlusstestslm/AbschlusstestSlmApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.abschlusstestslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbschlusstestSlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbschlusstestSlmApplication.class, args);
+    }
+
+}
