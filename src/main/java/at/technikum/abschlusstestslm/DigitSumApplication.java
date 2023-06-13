af0433b10045d7f39d6a1556ef2d5878bd548d52
Olibabababa <o1koller@gmail.com>
2023-06-13 10:48:58 +0200

    This commit adds a digitSum feature as well as a usage count that tracks how often it is used.

---
+++ src/main/java/at/technikum/abschlusstestslm/DigitSumApplication.java
@@ -0,0 +1,31 @@
+package at.technikum.abschlusstestslm;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DigitSumApplication{
+
+    private static int usage = 0;
+
+    @GetMapping("/api/digitsum")
+    public int getDigitSum (@RequestParam int number){
+        int digit;
+        int sum = 0;
+
+        while (number != 0) {
+            digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        usage++;
+        return sum;
+    }
+
+    @GetMapping("/api/digitsum/usage")
+    public static int usage(){
+        return usage;
+    }
+}
