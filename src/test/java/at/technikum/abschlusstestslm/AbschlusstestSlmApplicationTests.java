e26302dedb084ceaeb24420116e17adb2a11d85a
Olibabababa <o1koller@gmail.com>
2023-06-13 11:02:34 +0200

    This commit adds a digitSum feature test file

--- src/test/java/at/technikum/abschlusstestslm/AbschlusstestSlmApplicationTests.java
+++ src/test/java/at/technikum/abschlusstestslm/AbschlusstestSlmApplicationTests.java
@@ -2,12 +2,23 @@ package at.technikum.abschlusstestslm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AbschlusstestSlmApplicationTests {
 
     @Test
     void contextLoads() {
-    }
 
+        class CalculatorMessageControllerTest {
+
+            @Test
+            void getSumTest() {
+                int number = 9999;
+                assertEquals(10, DigitSumApplication.getDigitSum(number));
+            }
+        }
+    }
 }
